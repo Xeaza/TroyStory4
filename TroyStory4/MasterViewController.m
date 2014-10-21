af85d1f22f672c599dc8524e5aee17728b6cefc3
xeaza <taylorwrightsanson@gmail.com>
2014-10-21 11:32:43 -0500

    Added TableView Delegate Methods

--- TroyStory4/MasterViewController.m
+++ TroyStory4/MasterViewController.m
@@ -1,6 +1,8 @@
 #import "MasterViewController.h"
 
-@interface MasterViewController ()
+@interface MasterViewController () <UITableViewDataSource>
+
+@property NSArray *trojans;
 
 @end
 
@@ -9,6 +11,20 @@
 - (IBAction)onTrojanConquest:(UITextField *)sender
 {
     [sender resignFirstResponder];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSManagedObject *trojan = [self.trojans objectAtIndex:indexPath.row];
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textLabel.text = [trojan valueForKey:@"name"];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.trojans.count;
 }
 
 @end
